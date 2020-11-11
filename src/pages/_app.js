6f0d14e2238f8a4716ba350fcff641bf8ed71057
johncoryk <coryk0789@gmail.com>
2020-11-11 08:47:43 -0500

    added components folder and rearranged filesystem

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'theme-ui';
+import theme from '../styles/theme';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
