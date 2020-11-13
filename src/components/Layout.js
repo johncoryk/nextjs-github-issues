df78b8cb483cf6baf79048672c5722038310e3d3
johncoryk <coryk0789@gmail.com>
2020-11-13 15:53:00 -0500

    first bit of layout styling complete

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,73 @@
-import { css } from '@emotion/react';
-
 const Layout = ({ children }) => {
   return (
     <div
-      css={{
+      css={theme => ({
         display: 'flex',
         flexDirection: 'column',
-      }}
+        minHeight: '100vh',
+        color: theme.colors.black,
+      })}
     >
-      <header>
-        <h3>Github Issues</h3>
-        <nav>
-          <ul>
-            <li>About</li>
-            <li>Contact</li>
-            <li>
-              <a href='#'>Something there</a>
-            </li>
-          </ul>
-        </nav>
+      <header
+        css={theme => ({
+          backgroundColor: theme.colors.primary,
+          paddingTop: theme.spacing.medium,
+          paddingBottom: theme.spacing.medium,
+          marginBottom: theme.spacing.large,
+        })}
+      >
+        <div
+          css={theme => ({
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            margin: '0 auto',
+            maxWidth: theme.sizing.header,
+            width: '90vw',
+            alignItems: 'baseline',
+          })}
+        >
+          <h3>Github Issues</h3>
+          <nav>
+            <ul
+              css={theme => ({
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+
+                li: {
+                  marginLeft: theme.spacing.small,
+                },
+              })}
+            >
+              <li>About</li>
+              <li>Contact</li>
+              <li>
+                <a href='#'>Something there</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
-      <main>{children}</main>
-      <footer>
-        <h2>This is the footer</h2>
+      <main
+        css={theme => ({
+          maxWidth: theme.sizing.body,
+          width: '90vw',
+          margin: '0 auto',
+          flexGrow: 1,
+        })}
+      >
+        {children}
+      </main>
+      <footer
+        css={theme => ({
+          margin: '0 auto',
+          maxWidth: theme.sizing.header,
+          width: '90vw',
+          paddingBottom: theme.spacing.medium,
+        })}
+      >
+        <p>This is the footer content</p>
       </footer>
     </div>
   );
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,18 @@
 const theme = {
   colors: {
-    primary: 'red',
+    primary: 'teal',
+    secondary: '#777',
+    black: '#222',
+  },
+  spacing: {
+    small: '1rem',
+    medium: '2rem',
+    large: '4rem',
+    xlarge: '6rem',
+  },
+  sizing: {
+    header: '1100px',
+    body: '960px',
   },
 };
 
